bd38ef683b122c2fefd044061f2555bfdbaf3207
tukienhoa <tukienhoa1710@gmail.com>
2022-06-13 23:20:15 +0700

    Add signing transactions

--- src/main.js
+++ src/main.js
@@ -1,113 +1,19 @@
-const SHA256 = require('crypto-js/sha256');
+const {Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Transaction {
-    constructor(fromAddress, toAddress, amount) {
-        this.fromAddress = fromAddress;
-        this.toAddress = toAddress;
-        this.amount = amount;
-    }
-}
-
-class Block {
-    constructor(timestamp, transactions, previousHash = '') {
-        this.timestamp = timestamp;
-        this.transactions = transactions;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-    }
-
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
-    }
-
-    mineBlock(difficulty) {
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-        console.log("Block mined: " + this.hash);
-    }
-}
-
-class Blockchain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
-        this.pendingTransactions = [];
-        this.miningReward = 100;
-    }
-
-    createGenesisBlock() {
-        return new Block("01/01/2022", "Genesis Block", "0");
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    minePendingTransactions(miningRewardAddress) {
-        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
-        block.mineBlock(this.difficulty);
-
-        console.log("Block successfully mined!");
-        this.chain.push(block);
-
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ];
-    }
-
-    createTransaction(transaction) {
-        this.pendingTransactions.push(transaction);
-    }
-
-    getBalanceOfAddress(address) {
-        let balance = 0;
-
-        for (const block of this.chain) {
-            for (const transaction of block.transactions) {
-                if (transaction.fromAddress === address) {
-                    balance -= transaction.amount;
-                }
-
-                if (transaction.toAddress === address) {
-                    balance += transaction.amount;
-                }
-            }
-        }
-
-        return balance;
-    }
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
+const myKey = ec.keyFromPrivate('92ace4ab7c67ff6b424402cff6241d7995d55897d137288eff2e3b25facce847');
+const myWalletAddress = myKey.getPublic('hex');
 
 let myCoin = new Blockchain();
-myCoin.createTransaction(new Transaction('address1', 'address2', 100));
-myCoin.createTransaction(new Transaction('address2', 'address1', 50));
+
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+myCoin.addTransaction(tx1);
 
 console.log('\n Starting the miner...');
-myCoin.minePendingTransactions('my-address');
+myCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of me is: ', myCoin.getBalanceOfAddress('my-address'));
+console.log('\nBalance of me is: ', myCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('\n Starting the miner again...');
-myCoin.minePendingTransactions('my-address');
-
-console.log('\nBalance of me is: ', myCoin.getBalanceOfAddress('my-address'));
+console.log('Is chain valid?', myCoin.isChainValid());
