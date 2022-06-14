c0062d7c8bc77adfc80116f83f0731b88960c533
tukienhoa <tukienhoa1710@gmail.com>
2022-06-14 22:01:03 +0700

    Update genesis block

--- src/blockchain.js
+++ src/blockchain.js
@@ -76,7 +76,7 @@ class Blockchain {
     }
 
     createGenesisBlock() {
-        return new Block("01/01/2022", "Genesis Block", "0");
+        return new Block(Date.parse("01/01/2022"), [], "0");
     }
 
     getLatestBlock() {
